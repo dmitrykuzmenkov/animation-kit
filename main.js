9e466a8c1f280ca51c48a413579cfb65a7bf77e8
Dmitry Kuzmenkov <dmitry@wagh.ru>
2016-01-11 00:06:39 +0700

    Add dev code

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 require('./main.less');
+var domd = require('domd');
+var animation = require('./index.js');
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM loaded');
+  var element = document.querySelector('.js-animated-element');
+
+  var emit = domd(document.body);
+
+  emit.on('click', '.js-animate', function (e, t) {
+    var anim = t.getAttribute('data-animation');
+    animation(element).animate(anim);
+  });
+
+});
